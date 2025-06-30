f1d0732c738175869c478ea3a896e9b9bf5d9e6d
jay <jieun.p974@gmail.com>
2025-07-01 00:00:51 +0900

    repository, service 개발

--- src/main/kotlin/com/lecture/portfolio/domain/entity/Experience.kt
+++ src/main/kotlin/com/lecture/portfolio/domain/entity/Experience.kt
@@ -1,6 +1,14 @@
 package com.lecture.portfolio.domain.entity
 
-import jakarta.persistence.*
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToMany
 
 @Entity
 class Experience(
@@ -12,10 +20,11 @@ class Experience(
     endMonth: Int?,
     isActive: Boolean
 ) : BaseEntity() {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "experience_id")
-    var id: Long? = null // ?는 null허용이라는 뜻
+    var id: Long? = null
 
     var title: String = title
 
@@ -31,21 +40,19 @@ class Experience(
 
     var isActive: Boolean = isActive
 
-    @OneToMany(targetEntity = ExperienceDetail::class,
-        fetch = FetchType.LAZY,
-        cascade = [CascadeType.ALL])
+    @OneToMany(targetEntity = ExperienceDetail::class, fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
     @JoinColumn(name = "experience_id")
     var details: MutableList<ExperienceDetail> = mutableListOf()
 
-    fun getEndYearMoth(): String{
-        if(endYear == null || endMonth == null){
+    fun getEndYearMonth(): String {
+        if (endYear == null || endMonth == null) {
             return "Present"
         }
-        return "${endYear}.${endMonth}" //2025.02 같은 방식으로 return
+
+        return "${endYear}.${endMonth}"
     }
 
-    fun update(title: String, description: String, startYear: Int, startMonth: Int,
-               endYear: Int?, endMonth: Int?, isActive: Boolean){
+    fun update(title: String, description: String, startYear: Int, startMonth: Int, endYear: Int?, endMonth: Int?, isActive: Boolean) {
         this.title = title
         this.description = description
         this.startYear = startYear
@@ -55,8 +62,8 @@ class Experience(
         this.isActive = isActive
     }
 
-    fun addDetails(details: MutableList<ExperienceDetail>?){
-        if(details != null){
+    fun addDetails(details: MutableList<ExperienceDetail>?) {
+        if (details != null) {
             this.details.addAll(details)
         }
     }
--- src/main/kotlin/com/lecture/portfolio/presentation/service/PresentationService.kt
+++ src/main/kotlin/com/lecture/portfolio/presentation/service/PresentationService.kt
@@ -1,7 +1,41 @@
 package com.lecture.portfolio.presentation.service
 
+import com.lecture.portfolio.presentation.dto.IntroductionDTO
+import com.lecture.portfolio.presentation.dto.LinkDTO
+import com.lecture.portfolio.presentation.dto.ProjectDTO
+import com.lecture.portfolio.presentation.dto.ResumeDTO
+import com.lecture.portfolio.presentation.repository.PresentationRepository
+import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 
 @Service
-class PresentationService {
+class PresentationService (
+    private val presentationRepository: PresentationRepository
+){
+    @Transactional(readOnly = true)
+    fun getIntroductions(): List<IntroductionDTO> {
+        val introductions = presentationRepository.getActiveIntroductions()
+        return introductions.map { introduction -> IntroductionDTO(introduction) }
+    }
+    @Transactional(readOnly = true)
+    fun getLinks(): List<LinkDTO> {
+        val links = presentationRepository.getActiveLinks()
+        return links.map { link -> LinkDTO(link) }
+    }
+    @Transactional(readOnly = true)
+    fun getResume(): ResumeDTO {
+        val experiences = presentationRepository.getActiveExperiences()
+        val achievements = presentationRepository.getActiveAchievements()
+        val skills = presentationRepository.getActiveSkills()
+        return ResumeDTO(
+            experiences = experiences,
+            achievements = achievements,
+            skills = skills
+        )
+    }
+    @Transactional(readOnly = true)
+    fun getProjects(): List<ProjectDTO> {
+        val projects = presentationRepository.getActiveProjects()
+        return projects.map { project -> ProjectDTO(project) }
+    }
 }
