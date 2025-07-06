506373fb833befa6dcdc4529e34231c205fb14d3
jay <jieun.p974@gmail.com>
2025-07-06 23:37:59 +0900

    thymeleaf - 탬플릿 수정(projects, layout) 인터셉터

--- src/main/kotlin/com/lecture/portfolio/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/lecture/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,8 +1,24 @@
 package com.lecture.portfolio.presentation.interceptor
 
+import com.lecture.portfolio.domain.entity.HttpInterface
+import com.lecture.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import java.lang.Exception
 
 @Component
-class PresentationInterceptor {
-
+class PresentationInterceptor (
+    private val httpInterfaceRepository: HttpInterfaceRepository
+): HandlerInterceptor{
+    override fun afterCompletion(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        ex: Exception?
+    ) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
 }
--- src/main/kotlin/com/lecture/portfolio/presentation/interceptor/PresentationInterceptorConfiguration.kt
+++ src/main/kotlin/com/lecture/portfolio/presentation/interceptor/PresentationInterceptorConfiguration.kt
@@ -1,8 +1,16 @@
 package com.lecture.portfolio.presentation.interceptor
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class PresentationInterceptorConfiguration {
-
+class PresentationInterceptorConfiguration (
+    private val presentationInterceptor: PresentationInterceptor
+) : WebMvcConfigurer{
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(presentationInterceptor)
+            .addPathPatterns("/**") // 모든경로에 대해서 실행
+            .excludePathPatterns("/assets/**","/css/**","/js/**","/admin/**","/h2**","/favicon/ico","/error") // 제외할 경로들
+    }
 }
